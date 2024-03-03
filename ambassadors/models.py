e93055d8dd7c4eb94a8e353f5964a17f0f1bdd64
Lexxar91 <myratov91@yandex.ru>
2024-03-03 14:09:38 +0300

    del ammbasador

--- ambassadors/models.py
+++ ambassadors/models.py
@@ -3,7 +3,3 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 Ambassador = get_user_model()
-
-
-class AmbassadorProfile(models.Model):
-    user = models.OneToOneField(Ambassador, on_delete=models.CASCADE)
